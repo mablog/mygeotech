3c6de966795ab042cfdde6b57b62baf1bd281ab6
mablog <mohamed.bennama@gmail.com>
2023-03-19 15:45:26 +0100

    lqskdlqsd

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDom from "react-dom"
+import App from "./jsx/App"
+const app = document.getElementById("app")
+ReactDom.render(<App />, app)
---
+++ src/jsx/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <>
+        <div>Salam l'équip e, rani fort</div>
+        <p> jujuhghde</p>
+      </>
+    );
+  }
+  
+  export default App;
